854751a89788beef5ebf3e14ae93bf3b10c486cd
Joy Trinh <mongtuyen149@gmail.com>
2018-12-16 22:20:55 +0700

    Complete version 1

--- vigenere/Program.cs
+++ vigenere/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace vigenere
 {
@@ -10,57 +11,74 @@ namespace vigenere
     {
         static void Main(string[] args)
         {
-            string text = "";
-            for (int i = 0; i < args.Length - 1; i++)
-            {
-                text += args[i] + " ";
-            }
-            string keyString = args[args.Length - 1];
-            int key;
+            //string text = "";
+            //for (int i = 0; i < args.Length - 1; i++)
+            //{
+            //    text += args[i] + " ";
+            //}
+            //string keyString = args[args.Length - 1];
+            Console.WriteLine("Input text: ");
+            string text = Console.ReadLine();
+            Console.WriteLine("Input keyString: ");
+            string keyString = Console.ReadLine();
+            //verify that input contains only letters
+            bool success = Regex.IsMatch(keyString, @"^[a-zA-Z]+$");
 
-            //Check key
-            bool success = int.TryParse(keyString, out key);
             if (success)
             {
                 int n = text.Length;
-                int[] newText = new int[n];
+                int[] asciiValueOfText = new int[n];
                 char[] arr = new char[n];
 
-                //Convert from text to ASCII value:
-                for (int i = 0; i < n; i++)
+                int k = keyString.Length;
+                int[] asciiValueOfKeyString = new int[k];
+                //Get the index of each letter
+                //of keyString in the alphabet index (26 indexes)
+                //and then add each of value in an array
+                for (int i = 0; i < k; i++)
                 {
-                    var a = text[i];
-                    if (char.IsLetter(a))
-                    {
-                        var b = (int)a;
-                        if (char.IsUpper(a))
-                        {
-                            if (b <= (90 - key))
-                                newText[i] = b + key;
-                            else
-                                newText[i] = b + key - 26;
-                        }
-                        if (char.IsLower(a))
-                        {
-                            if (b <= (122 - key))
-                                newText[i] = b + key;
-                            else
-                                newText[i] = b + key - 26;
-                        }
-                    }
+                    var x = keyString[i];                    
+                    if (char.IsUpper(x))
+                        asciiValueOfKeyString[i] = x - 65;
                     else
-                        newText[i] = (int)a;
-                }
+                        asciiValueOfKeyString[i] = x - 97;
+                }                
+                    for (int i = 0; i < n; i++)
+                    {
+                        var a = text[i];
+                        var b = asciiValueOfKeyString[i%k];
+                        if (char.IsLetter(a))
+                        {
+                            if (char.IsUpper(a))
+                            {
+                                if (a <= (90 - b))
+                                    asciiValueOfText[i] = a + b;
+                                else
+                                    asciiValueOfText[i] = a + b - 26;
+                            }
+                            if (char.IsLower(a))
+                            {
+                                if (a <= (122 - b))
+                                    asciiValueOfText[i] = a + b;
+                                else
+                                    asciiValueOfText[i] = a + b - 26;
+                            }
+                        }
+                        else
+                            asciiValueOfText[i] = a;
+                    }    
+
+                  
                 //Convert form ASCII to char
                 for (int i = 0; i < n; i++)
                 {
-                    arr[i] = (char)newText[i];
+                    arr[i] = (char)asciiValueOfText[i];
                     Console.Write(arr[i]);
                 }
                 Console.ReadLine();
             }
             else
-                Console.WriteLine("Please input a valid key. A key must be a number.");
+                Console.WriteLine("Please input a valid key. A key must be a string.");
         }
     }
 }
